44f89b7d7a55921e07837d2c7af9bdfc8befa7d8
Enze Chi <enze.chi@gmail.com>
2018-11-11 20:16:23 +1100

    Remove task-juggler formula

--- Formula/task-juggler.rb
+++
@@ -1,8 +0,0 @@
-class TaskJuggler < Formula
-  desc "The TaskJuggler Project Management Software"
-  homepage "http://taskjuggler.org/"
-
-  def install
-    system "gem", "install", "taskjuggler", "--version", "3.6.0", "--install-dir", prefix
-  end
-end
