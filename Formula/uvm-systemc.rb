a0aa1021c11f02c6e4e79fc9801bb407b37b34b5
Enze Chi <ezchi@Enzes-MBP.hub>
2020-02-02 15:00:39 +1100

    Add UVM-SystemC formula

---
+++ Formula/uvm-systemc.rb
@@ -0,0 +1,12 @@
+class UVMSystemC < Formula
+  desc "Universal Verification Methodology (UVM) in SystemC"
+  homepage "https://github.com/ezchi/uvm-systemc"
+  url "https://github.com/ezchi/uvm-systemc.git", :tag => "1.0-beta-cmake-2", :revision => "0f288104533a409f9c01d3d34bc69c0e32f7186c"
+  depends_on "cmake" => :build
+  depends_on "llvm" => :build
+
+  def install
+    system "cmake", "-Brelease", "-H.", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+    system "cmake", "--build", "release", "--target", "install"
+  end
+end
