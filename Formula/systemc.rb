6ecd486a850e811103b6d0b371d5863df751132d
Enze Chi <ezchi@Enzes-MBP.hub>
2020-02-02 19:43:07 +1100

    Add doxygen and graphviz as SystemC dependencies

--- Formula/systemc.rb
+++ Formula/systemc.rb
@@ -4,6 +4,8 @@ class Systemc < Formula
   url "https://github.com/ezchi/systemc.git", :tag => "v2.3.3", :revision => "e30ff935206887c35c81de837e244537fb3188d7"
   depends_on "cmake" => :build
   depends_on "llvm" => :build
+  depends_on "doxygen" => :build
+  depends_on "graphviz" => :build
 
   def install
     ENV["CLANG_BASE_PATH"] = Formula["llvm"].prefix
