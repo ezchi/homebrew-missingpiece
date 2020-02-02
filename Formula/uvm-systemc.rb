2534cc96ca73d40963dcdecdcbd2687854044906
Enze Chi <ezchi@Enzes-MBP.hub>
2020-02-02 15:05:22 +1100

    Rename UVM-SystemC formula class name

--- Formula/uvm-systemc.rb
+++ Formula/uvm-systemc.rb
@@ -1,4 +1,4 @@
-class UVMSystemc < Formula
+class UvmSystemc < Formula
   desc "Universal Verification Methodology (UVM) in SystemC"
   homepage "https://github.com/ezchi/uvm-systemc"
   url "https://github.com/ezchi/uvm-systemc.git", :tag => "1.0-beta-cmake-2", :revision => "0f288104533a409f9c01d3d34bc69c0e32f7186c"
