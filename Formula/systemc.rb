10ef4be9fa82ee465e0c5965a39dfb6be0332234
Enze Chi <ezchi@Enzes-MBP.hub>
2020-02-02 14:17:48 +1100

    Add systemc formula

---
+++ Formula/systemc.rb
@@ -0,0 +1,12 @@
+class Systemc < Formula
+  desc "Core SystemC language and examples"
+  homepage "https://github.com/ezchi/systemc"
+  url "https://github.com/ezchi/systemc.git", :tag => "v2.3.3", :revision => "e30ff935206887c35c81de837e244537fb3188d7"
+  depends_on "cmake" => :build
+  depends_on "llvm" => :build
+
+  def install
+    system "cmake", "-Brelease", "-H."
+    system "cmake", "--build", "release", "--target", "install"
+  end
+end
