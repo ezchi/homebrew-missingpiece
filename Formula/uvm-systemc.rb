fc0fb70f4b5552f8f53abf465b369501bc425fa7
Enze Chi <ezchi@Enzes-MBP.hub>
2020-02-02 15:09:25 +1100

    Fix revision of UVM-SystemC formula

--- Formula/uvm-systemc.rb
+++ Formula/uvm-systemc.rb
@@ -1,7 +1,7 @@
 class UvmSystemc < Formula
   desc "Universal Verification Methodology (UVM) in SystemC"
   homepage "https://github.com/ezchi/uvm-systemc"
-  url "https://github.com/ezchi/uvm-systemc.git", :tag => "1.0-beta-cmake-2", :revision => "0f288104533a409f9c01d3d34bc69c0e32f7186c"
+  url "https://github.com/ezchi/uvm-systemc.git", :tag => "1.0-beta-cmake-2", :revision => "21c834fcf15f0ca5644b5c34c2ecbc2143c37d69"
   depends_on "cmake" => :build
   depends_on "llvm" => :build
 
