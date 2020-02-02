b7abfb9116e09e7ecffa7d3ac7470491e8a2c12a
Enze Chi <ezchi@Enzes-MBP.hub>
2020-02-02 19:36:33 +1100

    Add dependency and compile flags for UVM-SystemC
    - Add SystemC as dependency
    - Use llvm/clang++ as CXX compiler

--- Formula/uvm-systemc.rb
+++ Formula/uvm-systemc.rb
@@ -4,9 +4,15 @@ class UvmSystemc < Formula
   url "https://github.com/ezchi/uvm-systemc.git", :tag => "1.0-beta-cmake-2", :revision => "21c834fcf15f0ca5644b5c34c2ecbc2143c37d69"
   depends_on "cmake" => :build
   depends_on "llvm" => :build
+  depends_on "systemc" => :build
 
   def install
-    system "cmake", "-Brelease", "-H.", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+    ENV["CLANG_BASE_PATH"] = Formula["llvm"].prefix
+    system "cmake", "-Brelease", "-H.",
+           "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+           "-DBUILD_SOURCE_DOCUMENTATION=ON",
+           "-DCMAKE_BUILD_TYPE=Release",
+           "-DCMAKE_CXX_COMPILER=clang++"
     system "cmake", "--build", "release", "--target", "install"
   end
 end
