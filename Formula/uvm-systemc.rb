ed0ab6465c77c30a123cb1ef6349ed5636db2203
Enze Chi <enze.chi@gmail.com>
2021-01-17 16:58:01 +1100

    Bump uvm-systemc to 1.0-beta2-cmake-4

--- Formula/uvm-systemc.rb
+++ Formula/uvm-systemc.rb
@@ -1,13 +1,14 @@
 class UvmSystemc < Formula
   desc "Universal Verification Methodology (UVM) in SystemC"
   homepage "https://github.com/ezchi/uvm-systemc"
-  url "https://github.com/ezchi/uvm-systemc.git", :tag => "1.0-beta-cmake-2", :revision => "21c834fcf15f0ca5644b5c34c2ecbc2143c37d69"
+  url "https://github.com/ezchi/uvm-systemc.git", :tag => "1.0-beta2-cmake-4", :revision => "b677c7e9776afff8d6b91f3437a5da0152ee2c91"
   depends_on "cmake" => :build
   depends_on "llvm" => :build
   depends_on "ezchi/homebrew-missingpiece/systemc" => :build
 
   def install
     ENV["CLANG_BASE_PATH"] = Formula["llvm"].prefix
+    ENV["SYSTEMC_HOME"] = Formula["ezchi/homebrew-missingpiece/systemc"].prefix
     system "cmake", "-Brelease", "-H.",
            "-DCMAKE_INSTALL_PREFIX=#{prefix}",
            "-DBUILD_SOURCE_DOCUMENTATION=ON",
