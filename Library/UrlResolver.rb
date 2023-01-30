613d497632053a31b344244241c7eaffc1c9711f
Enze Chi <Enze.Chi@gmail.com>
2023-01-30 20:55:39 +1100

    Add emacs-plus formula

---
+++ Library/UrlResolver.rb
@@ -0,0 +1,22 @@
+TAP_OWNER = "d12frosted"
+TAP_REPO = "emacs-plus"
+
+class UrlResolver
+  def initialize(version, mode)
+    name = "#{TAP_REPO}@#{version}"
+    tap = Tap.new TAP_OWNER, TAP_REPO
+    @version = version
+    @formula_root =
+      mode == "local" || !tap.installed? ?
+        Dir.pwd :
+        (tap.path.to_s.delete_suffix "/Formula/#{name}.rb")
+  end
+
+  def patch_url name
+    "#{@formula_root}/patches/emacs-#@version/#{name}.patch"
+  end
+
+  def icon_url name
+    "#{@formula_root}/icons/#{name}.icns"
+  end
+end
