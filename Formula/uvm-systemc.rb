587bf30a085a59b9e008ef77d0f321d81dcec5be
Enze Chi <Enze.Chi@gmail.com>
2023-05-15 21:20:53 +1000

    Bump UVM-SystemC to 1.0-beta5-cmake-1

--- Formula/uvm-systemc.rb
+++ Formula/uvm-systemc.rb
@@ -1,8 +1,8 @@
 class UvmSystemc < Formula
   desc "Universal Verification Methodology (UVM) in SystemC"
   homepage "https://github.com/ezchi/uvm-systemc"
-  url "https://github.com/ezchi/uvm-systemc.git", tag: "1.0-beta3-cmake-3", revision: "2bf09868cc8c1859e6a26746533ac78a0666c25a"
-  version "1.0-beta3-cmake-3"
+  url "https://github.com/ezchi/uvm-systemc.git", tag: "1.0-beta5-cmake-1", revision: "3f9954745cedaea5a75293c8e9923502bf74d67e"
+  version "1.0-beta5-cmake-1"
   bottle do
     root_url "https://github.com/ezchi/homebrew-missingpiece/raw/master/Bottles"
     sha256 cellar: :any_skip_relocation, catalina: "971caf61374c88dc7010b5f0b741597fda79e68aea5f2df276594b15dea7c0a0"
