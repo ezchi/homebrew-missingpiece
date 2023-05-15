69d4a42869b5754620c6aa9c314e3a2eca0a928a
Enze Chi <Enze.Chi@gmail.com>
2023-05-15 20:24:34 +1000

    Bump UVM-SystemC to support Apple Silicon M2

--- Formula/uvm-systemc.rb
+++ Formula/uvm-systemc.rb
@@ -1,8 +1,8 @@
 class UvmSystemc < Formula
   desc "Universal Verification Methodology (UVM) in SystemC"
   homepage "https://github.com/ezchi/uvm-systemc"
-  url "https://github.com/ezchi/uvm-systemc.git", tag: "1.0-beta3-cmake-2", revision: "3368d7a1756514d7edd3415282dea812f4a5512a"
-  version "1.0-beta3-cmake-2"
+  url "https://github.com/ezchi/uvm-systemc.git", tag: "1.0-beta3-cmake-3", revision: "2bf09868cc8c1859e6a26746533ac78a0666c25a"
+  version "1.0-beta3-cmake-3"
   bottle do
     root_url "https://github.com/ezchi/homebrew-missingpiece/raw/master/Bottles"
     sha256 cellar: :any_skip_relocation, catalina: "971caf61374c88dc7010b5f0b741597fda79e68aea5f2df276594b15dea7c0a0"
